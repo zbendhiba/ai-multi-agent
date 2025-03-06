028ba513d9157e7ae5c61cf7e7e9e935eb70cdf8
Bruno Meseguer <bruno.meseguer@gmail.com>
2025-03-06 16:07:30 +0000

    first commit

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Run with:
+camel run --logging-category=io.vertx.core.impl.BlockedThreadChecker=ERROR *
+
+Open a browser:
+try:
+http://localhost:8080/camel/article/sport-is-healthy
---
+++ bindings.java
@@ -0,0 +1,60 @@
+//DEPS dev.langchain4j:langchain4j-open-ai:0.33.0
+
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.PropertyInject;
+import org.apache.camel.builder.RouteBuilder;
+
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+import static java.time.Duration.ofSeconds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.builder.ExchangeBuilder;
+
+public class bindings extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // Routes are loaded from YAML files
+    }
+
+    private static String LLM_URL;
+
+    @PropertyInject("llm.url")
+    public void setLlmUrl(String url) {
+        LLM_URL = url;
+    }
+
+    public static String getLlmUrl() {
+        return LLM_URL;
+    }
+
+    @BindToRegistry(lazy=true)
+    public static ChatLanguageModel chatModel(){
+
+        ChatLanguageModel model = OpenAiChatModel.builder()
+            .apiKey("EMPTY")
+            // .modelName("qwen2.5:0.5b-instruct")
+            // .modelName("qwen2.5:7b-instruct")
+            .modelName("qwen2.5:3b-instruct")
+            // .modelName("qwen2.5-coder:3b")
+            .baseUrl("http://"+getLlmUrl()+"/v1/")
+            .temperature(0.0)
+            .timeout(ofSeconds(180))
+            .logRequests(true)
+            .logResponses(true)
+            .build();
+
+        return model;
+    }
+
+}
